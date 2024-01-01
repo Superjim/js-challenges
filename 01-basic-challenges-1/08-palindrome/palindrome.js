2ee8942436425c7be00240a941f8440aadbd8888
James Jenkinson <jamesjenkinson10@gmail.com>
2024-01-01 06:21:20 +0000

    2

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,6 @@
-function isPalindrome() {}
+function isPalindrome(s) {
+  string = s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+  return string === string.split("").reverse().join("");
+}
 
 module.exports = isPalindrome;
--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,11 @@
-function removeDuplicates() {}
+function removeDuplicates(nums) {
+  let freq = {};
+  for (let num of nums) {
+    if (!freq.hasOwnProperty(num)) {
+      freq[num] = num;
+    }
+  }
+  return Object.values(freq);
+}
 
 module.exports = removeDuplicates;
