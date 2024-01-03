ebf204d65ec7732c3a76d7c9c43bbdb029b9ed40
James Jenkinson <jamesjenkinson10@gmail.com>
2024-01-03 08:48:52 +0000

    missing letters

--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,3 +1,15 @@
-function findMissingLetter() {}
+function findMissingLetter(chars) {
+  let charCodes = chars.map((char) => char.charCodeAt(0));
+  charCodes.sort((a, b) => a - b);
+
+  let fullArray = [];
+  for (let i = charCodes[0]; i <= charCodes[charCodes.length - 1]; i++) {
+    fullArray.push(i);
+  }
+
+  let missingCode = fullArray.find((code) => !charCodes.includes(code));
+
+  return String.fromCharCode(missingCode);
+}
 
 module.exports = findMissingLetter;
