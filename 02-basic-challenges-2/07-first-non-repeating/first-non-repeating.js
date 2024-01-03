70a9a29f449990d588bb68e247080992187ffb1f
James Jenkinson <jamesjenkinson10@gmail.com>
2024-01-03 09:06:38 +0000

    non repeating

--- 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
+++ 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
@@ -1,3 +1,12 @@
-function findFirstNonRepeatingCharacter() {}
+function findFirstNonRepeatingCharacter(str) {
+  let freq = {};
+  for (let char of str) {
+    freq[char] = (freq[char] || 0) + 1;
+  }
+  for (let obj of Object.entries(freq)) {
+    if (obj[1] === 1) return obj[0];
+  }
+  return null;
+}
 
 module.exports = findFirstNonRepeatingCharacter;
