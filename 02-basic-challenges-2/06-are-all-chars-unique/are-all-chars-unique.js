474b8ed9cec28b0db580514d91ab346b24e0e260
James Jenkinson <jamesjenkinson10@gmail.com>
2024-01-03 09:02:58 +0000

    unique

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,11 @@
-function areAllCharactersUnique() {}
-
+function areAllCharactersUnique(nums) {
+  let freq = {};
+  for (let num of nums) {
+    freq[num] = (freq[num] || 0) + 1;
+  }
+  for (let obj of Object.entries(freq)) {
+    if (obj[1] > 1) return false;
+  }
+  return true;
+}
 module.exports = areAllCharactersUnique;
